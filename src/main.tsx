e8e8d5092065095e5671b73bace8b67639575c32
jeyvpn <62214854+jeyvpn@users.noreply.github.com>
2025-11-01 18:08:35 +0330

    5

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cc">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  base: '/cc/',  // ← این مهمه! برای GitHub Pages
-  build: {
-    outDir: 'dist',  // خروجی build
-    assetsDir: 'assets',
-    sourcemap: true,  // برای debug
-  }
+  base: '/cc/'
 })
